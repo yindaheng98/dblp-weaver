857b177aaa98373a547cba568e2c547b03e2d8db
yindaheng98 <yindaheng98@gmail.com>
2022-10-28 20:00:57 +0800

    summary the detail of edge

--- main.py
+++ main.py
@@ -53,6 +53,25 @@ blacklist = [
 ]
 
 
+def construct_detail(publications):
+    detail = {}
+
+    def get_detail(pub, ccf):
+        return dict(
+            year=pub.year(), CCF=ccf, journal=pub.journal(),  # title=pub.title(), # too large
+            authors=", ".join(str(author) for author in pub.authors()))
+
+    for publication in publications:
+        detail[publication.key()] = get_detail(publication, 'N')
+    for publication in filter_publications_by_journals(publications, CCF_A):
+        detail[publication.key()] = get_detail(publication, 'A')
+    for publication in filter_publications_by_journals(publications, CCF_B):
+        detail[publication.key()] = get_detail(publication, 'B')
+    for publication in filter_publications_by_journals(publications, CCF_C):
+        detail[publication.key()] = get_detail(publication, 'C')
+    return detail
+
+
 class GG(Graph):
     def filter_publications_at_crawler(self, publications):
         publications = filter_publications_after(publications, 2019)
@@ -69,24 +88,26 @@ class GG(Graph):
         return publications
 
     def summary_person(self, person, publications):
-        detail = {}
-
-        def get_detail(pub, ccf):
-            return dict(
-                year=pub.year(), CCF=ccf, journal=pub.journal(),  # title=pub.title(), # too large
-                authors=", ".join(str(author) for author in pub.authors()))
-
-        for publication in person.publications():
-            detail[publication.key()] = get_detail(publication, 'N')
-        for publication in filter_publications_by_journals(person.publications(), CCF_A):
-            detail[publication.key()] = get_detail(publication, 'A')
-        for publication in filter_publications_by_journals(person.publications(), CCF_B):
-            detail[publication.key()] = get_detail(publication, 'B')
-        for publication in filter_publications_by_journals(person.publications(), CCF_C):
-            detail[publication.key()] = get_detail(publication, 'C')
         return dict(
             **super().summary_person(person, publications),
-            detail=detail,
+            detail=construct_detail(person.publications()),
+        )
+
+    def summary_cooperation(self, a, b, publications):
+        all_publications = {}
+        for pub in a.publications():
+            if pub.key() in all_publications:
+                continue
+            if b.pid() in list(author.pid() for author in pub.authors()):
+                all_publications[pub.key()] = pub
+        for pub in b.publications():
+            if pub.key() in all_publications:
+                continue
+            if a.pid() in list(author.pid() for author in pub.authors()):
+                all_publications[pub.key()] = pub
+        return dict(
+            **super().summary_cooperation(a, b, publications),
+            detail=construct_detail(all_publications.values()),
         )
 
 
