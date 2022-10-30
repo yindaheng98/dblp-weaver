e3b27a7c21a610a1dce0fcfe4181b99610410967
yindaheng98 <yindaheng98@gmail.com>
2022-10-30 15:29:04 +0800

    save

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ video_kw = {
 }
 
 # 超分的关键词
-sr_kw = {"super-resolution",
-         "denoise", "denoising", "deblur", "deblurring", "dehaze", "dehazing", "restoration", "restore", "restoring",
+sr_kw = {"denoise", "denoising", "deblur", "deblurring", "dehaze", "dehazing", "restoration", "restore", "restoring",
          "interpolation", "interpolate", "inpaint", "inpainting",
-         "enhance", "enhancement", "neural", "deep", "learn", "learning", "intelligent"}
+         "enhance", "enhancement", "neural", "deep", "learn", "learning", "intelligent", "transformer"}
+sr_kw2 = {{"super"}, {"resolution"}}
+# 模型的关键词
+model_kw = {"model", "models", "transformer", "transformers"}
+model_kw2 = {{"pre"}, {"train", "trained", "training"}}
 
 # 传输的关键词
 trans_kw = {"stream", "streaming", "delivery", "deliver", "cached", "cache", "caching",
@@ -24,6 +27,14 @@ trans_kw = {"stream", "streaming", "delivery", "deliver", "cached", "cache", "ca
 
 # 视频处理的关键词
 proc_kw = {"processing", "analytics"}
+
+# 优化的关键词
+opti_kw = {"optimize", "optimization",
+           "quantize", "quantization",
+           "compress", "compression",
+           "distillate", "distilling", "distillation",
+           "normalization", "real-time"}
+opti_kw2 = {{"real"}, {"time"}}
 
 # 应用的关键词
 app_kw = {"communication", "communicate", "conference", "conferencing", "stream", "streaming", "crowdcast"}
@@ -38,6 +49,11 @@ edge_comp_kw = edge_kw.union(comp_kw)
 keywords = Keywords()
 keywords.add_rule_list(
     *list(product(video_kw, sr_kw)),
+    *list(product(video_kw, *sr_kw2)),
+    *list(product(video_kw, model_kw)),
+    *list(product(video_kw, *model_kw2)),
+    *list(product(video_kw, opti_kw)),
+    *list(product(video_kw, *opti_kw2)),
     *list(product(video_kw, trans_kw)),
     *list(product(video_kw, proc_kw)),
     *list(product(video_kw, app_kw)),
