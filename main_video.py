2ceac4832b2ae715dd0f27f1e496f638fcb4674f
yindaheng98 <yindaheng98@gmail.com>
2022-10-30 16:13:05 +0800

    单独搞视频和优化的

---
+++ main_video.py
@@ -0,0 +1,26 @@
+from dblp_crawler import *
+from dblp_crawler.data import CCF_A
+from dblp_crawler.keyword import *
+from example import main
+from main import blacklist
+
+# 视频的关键词
+keywords = Keywords()
+keywords.add_word_rules(
+    "video", "live", "livecast", "livecast", "crowdcast", "crowdcasting",
+    "resolution", "360", "vr", "camera", "hdr", 'uhd', "VSR"
+)
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(
+        summary_path="summary_video.json",
+        paper_path="papers_video.txt",
+        keywords=keywords,
+        blacklist=blacklist,
+        pid_list=[],
+        journal_list=CCF_A
+    ))
