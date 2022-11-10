a6441702a7919bb886eb9a3f07498699f37c800a
yindaheng98 <yindaheng98@gmail.com>
2022-11-10 16:15:55 +0800

    gather

--- js/common.js
+++ js/common.js
@@ -68,21 +68,23 @@ function get_line_option(id, line_data) {
 function get_pub_text(id, person_data, pub_data) {
     return person_data[id] + "\n\n" + pub_data[id].join('\n\n')
 }
+function get_person_raw_data(id, raw_data) {
+    return raw_data[id]
+}
 
 ccfpie = echarts.init(document.getElementById("ccfpie"));
 conpie = echarts.init(document.getElementById("conpie"));
 line = echarts.init(document.getElementById("line"));
 pub = document.getElementById("publications")
+var person_raw_data;
 function change_person(id) {
     ccfpie.setOption(get_ccfpie_option(id, ccfpie_data))
     conpie.setOption(get_conpie_option(id, conpie_data))
     line.setOption(get_line_option(id, line_data))
     pub.innerText = get_pub_text(id, person_data, pub_data)
+    person_raw_data = get_person_raw_data(id, raw_data)
 }
 
-function get_person_raw_data(id, raw_data) {
-    return raw_data[id]
-}
 function search_person_raw_data(year, ccf, journal, person_raw_data) {
     for (raw in person_raw_data) {
 
