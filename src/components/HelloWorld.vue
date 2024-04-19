5715abc54dab6c855e9b9f30991d72978b12266f
yindaheng98 <yindaheng98@163.com>
2024-04-18 22:02:54 -0700

    strict type

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import NeoVis from 'neovis.js'
+import { type NumberOrInteger } from 'neovis.js'
+import * as Neo4jTypes from 'neo4j-driver'
 
 const viz = ref()
 const serverUrl = import.meta.env.VITE_NEO4J_SERVER_URL
@@ -9,7 +11,7 @@ const serverPassword = import.meta.env.VITE_NEO4J_SERVER_PASSWORD
 
 onMounted(() => {
   let neoviz = new NeoVis({
-    containerId: 'viz',
+    containerId: viz.value.id,
     neo4j: {
       serverUrl: serverUrl,
       serverUser: serverUser,
@@ -20,7 +22,8 @@ onMounted(() => {
         label: 'name',
         [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
           function: {
-            title: (node) => NeoVis.objectToTitleHtml(node, ['name'])
+            title: (node: Neo4jTypes.Node<NumberOrInteger>) =>
+              NeoVis.objectToTitleHtml(node, ['name'])
           },
           cypher: {
             value: 'MATCH (a:Person{id:$id})-[:WRITE]->(p:Publication) RETURN COUNT(p)'
