38e02d7aed24709e681255c9dba1ca78f2f01056
yindaheng98 <yindaheng98@163.com>
2024-05-11 16:11:52 -0700

    query from neo4j

---
+++ src/components/PapersByAuthor.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart } from 'echarts/charts'
+import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
+import VChart, { THEME_KEY } from 'vue-echarts'
+import { ref, provide, computed } from 'vue'
+import neo4j from 'neo4j-driver'
+import { type Record } from 'neo4j-driver'
+import { type IdType } from 'vis-network'
+import { serverUrl, serverUser, serverPassword } from './connection'
+
+const props = defineProps<{ id: IdType }>()
+const driver = neo4j.driver(serverUrl, neo4j.auth.basic(serverUser, serverPassword))
+const papers = ref<Record[]>([])
+const error = ref(null)
+driver
+  .session({ database: 'neo4j' })
+  .run(
+    `MATCH (a:Person) WHERE id(a)=$id
+  MATCH (a)-[:WRITE]->(p:Publication)
+  MATCH (j:Journal)<-[:PUBLISH]-(p)
+  RETURN p,j`,
+    { id: props.id }
+  )
+  .then((result) => {
+    papers.value = result.records
+  })
+  .catch((err) => {
+    error.value = err
+  })
+</script>
+
+<template>
+  <div v-if="error">Oops! Error encountered: {{ error }}</div>
+  <div v-else-if="papers">{{ papers }}</div>
+</template>
+
+<style scoped>
+.chart {
+  height: 400px;
+}
+</style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,6 +6,7 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 import StatisticBar from './StatisticBar.vue'
+import PapersByAuthor from './PapersByAuthor.vue'
 </script>
 
 <template>
@@ -24,20 +25,7 @@ import StatisticBar from './StatisticBar.vue'
     </template>
     <template #heading>Tooling</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
+    <PapersByAuthor :id="22" />
   </WelcomeItem>
 
   <WelcomeItem>
