48aa72affb4632f1a17be70653e89df5562a44e6
yindaheng98 <yindaheng98@gmail.com>
2024-05-12 22:25:58 -0700

    reorganize

--- src/components/PaperCitesGraph.vue
+++ src/components/PaperCitesGraph.vue
@@ -7,18 +7,15 @@ import { stateAuthorPapersStat, statePaperList } from './state'
 const props = defineProps<{ id: IdType, limit: number }>()
 
 const cyphers = computed(() => {
+  const Cyphers: string[] = []
+  Cyphers.push(`MATCH (p:Publication) WHERE id(p) = ${props.id} RETURN p`) // The first node
   const GET_CIT = `MATCH (pc:Publication)-[c:CITE]->(p:Publication) WHERE id(p) = ${props.id}` // pc is citations
   const GET_REF = `MATCH (pr:Publication)<-[r:CITE]-(p:Publication) WHERE id(p) = ${props.id}` // pr is references
   const GET_CIT_CIT = `${GET_CIT} OPTIONAL MATCH (cc:Publication)-[:CITE]->(pc)` // citations' citations
   const GET_REF_CIT = `${GET_REF} OPTIONAL MATCH (cc:Publication)-[:CITE]->(pr)` // references' citations
-  return [`MATCH (p:Publication) WHERE id(p) = ${props.id} RETURN p`, // The first node
-  `${GET_CIT_CIT}
-  WITH COUNT(cc) AS pcc, p, c, pc ORDER BY pcc LIMIT ${props.limit}
-  RETURN p, c, pc`, // citations
-  `${GET_REF_CIT}
-  WITH COUNT(cc) AS prc, p, r, pr ORDER BY prc LIMIT ${props.limit}
-  RETURN p, r, pr`, // references
-  ]
+  Cyphers.push(`${GET_CIT_CIT} WITH COUNT(cc) AS pcc, p, c, pc ORDER BY pcc LIMIT ${props.limit} RETURN p, c, pc`) // citations
+  Cyphers.push(`${GET_REF_CIT} WITH COUNT(cc) AS prc, p, r, pr ORDER BY prc LIMIT ${props.limit} RETURN p, r, pr`) // references
+  return Cyphers
 })
 </script>
 
